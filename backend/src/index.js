295c66f8aa7dbafe3a35c3cd66454139d2adf7f5
Abhishek Kumar Singh <jai123shriganesh@gmail.com>
2025-05-14 12:18:42 +0530

    updated index.js for deployment

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,13 +1,13 @@
-import express from 'express'
-import cors from 'cors'
-import { createServer } from 'http'
-import { Server } from 'socket.io'
+import express from 'express';
+import cors from 'cors';
+import { createServer } from 'http';
+import { Server } from 'socket.io';
 import { CLIENT_URL, PORT } from './config/veriables.js';
 import { connectDB } from './config/dbConfig.js';
 import userRouter from './routes/v1/user.route.js';
 import swaggerDocs from '../swagger.js';
 import swaggerUi from 'swagger-ui-express';
-import cookieParser from "cookie-parser";
+import cookieParser from 'cookie-parser';
 import pollRouter from './routes/v1/poll.route.js';
 import { handlePollSocket } from './socket/poll.socket.js';
 import voteRouter from './routes/v1/vote.route.js';
@@ -15,36 +15,33 @@ import voteRouter from './routes/v1/vote.route.js';
 const app = express();
 const httpServer = createServer(app);
 
-app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
-
 const io = new Server(httpServer, {
-    cors: {
-        origin: CLIENT_URL,
-        credentials: true
-    }
-})
-
+  cors: {
+    origin: CLIENT_URL,
+    credentials: true,
+  },
+});
 
 handlePollSocket(io);
 
-
 app.use(cookieParser());
-app.use(cors(
-    {
-        origin: CLIENT_URL,
-        credentials: true,
-    }
-))
-app.use(express.json())
-app.get("/ping", (_req, res) => {
-    res.json({ message: "pong" })
-})
+app.use(
+  cors({
+    origin: CLIENT_URL,
+    credentials: true,
+  })
+);
+app.use(express.json());
+app.get('/ping', (_req, res) => {
+  res.json({ message: 'pong' });
+});
 
-app.use("/api/v1/poll", pollRouter);
-app.use("/api/v1/user", userRouter);
-app.use("/api/v1/vote", voteRouter);
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+app.use('/api/v1/poll', pollRouter);
+app.use('/api/v1/user', userRouter);
+app.use('/api/v1/vote', voteRouter);
 
 await connectDB();
 httpServer.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`)
-})
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
